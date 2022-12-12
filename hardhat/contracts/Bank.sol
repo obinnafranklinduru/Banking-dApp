28f9ca2fcd30c67f36c404e8e135f2074df4e106
obinnafranklinduru <obinnafranklinduru.com>
2022-12-12 12:24:35 +0100

    initial commit

--- hardhat/contracts/Bank.sol
+++ hardhat/contracts/Bank.sol
@@ -1,12 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@op"
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-contract Bank is ERC20, Ownable {
+contract Bank {
+    // The owner of the contract, which will be initialized when we deploy the contract.
+    address public owner;
+    // This will store the symbol of the token and allow for depositing in this contract.
+    bytes32[] public whiteListedSymbols;
 
+    // To track the token symbol to the address where the token contract is deposited
+    mapping(bytes32 => address) public whiteListedTokens;
+    // This stores the token balances deposited by each address.
+    mapping(address => mapping(bytes32 => uint256)) public balance;
+
+    // The constructor initializes the owner as the caller address of this contract.
     constructor() ERC20("", "") {
         owner = msg.sender;
     }
